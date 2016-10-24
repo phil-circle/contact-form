240836544179e2863756333b43ec1d1049c2dee6
phil-circle <phil-circle@users.noreply.github.com>
2016-10-24 17:49:44 +0100

    updated with php mail handling

--- form-handler.php
+++ form-handler.php
@@ -4,14 +4,27 @@
 
 if ($_POST){
 	// we have form data
-	
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$phone = $_POST['phone'];
+	$message = $_POST['message'];
+	$from = 'My contact form';
+	$to = 'philipfnelms@gmail.com';
+	$subject = 'Message from my contact form';
+
+	$body = "Hi, we've had a form submission.\n From: $name\n Email: $email\n Phone: $phone\n Message: $message";
+
 	// format the data into a message
 	$email_message = "Hi, we've had a form submission, see it below.";
 	$email_message = $email_message . "Name " . $_POST['name'];
-	
+
 	// now send email
-	mail("oliward@gmail.com", "Contact form enquiry", $email_message);
-	
+	if (mail($to, $subject, $body)) {
+		$result='<div class="alert alert-success">Thankyou!</div>';
+	} else {
+		$result='<div class="alert alert-danger">Sorry. Error.</div>';
+	}
+
 	// now forward user to thank you page
 	header("Location: thank-you.html");
 	exit;
